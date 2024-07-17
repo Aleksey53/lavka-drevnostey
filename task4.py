d7d4bf3c7ba791dd921237c07a38bdb2615f2562
Aleksey53 <vmax7777@yandex.ru>
2024-07-17 20:35:05 +0300

    Add files via upload

---
+++ task4.py
@@ -0,0 +1,7 @@
+                        # " 4or program "
+print(int(13.42 // 1)),
+print(int(float(100*(42.13 % 42))))
+print(int(float(100*(13.42 % 13))))
+print(int(42.13 // 1))
+print(((int(float(100*(13.42 % 13)))) == (int(42.13 // 1))) or (int(13.42 // 1)) == (int(float(100*(42.13 % 42)))))
+print(((int(float(100*(13.42 % 13)))) != (int(42.13 // 1))) and (int(13.42 // 1)) == (int(float(100*(42.13 % 42)))))
