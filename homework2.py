a557b483de1478bc89a33053c7eeb2c5fd774767
Aleksey53 <vmax7777@yandex.ru>
2024-07-22 23:14:50 +0300

    Delete homework2.py

--- homework2.py
+++
@@ -1,5 +0,0 @@
-homework = 12
-hours_for_homework = 1.5
-name = 'Paython'
-time_for_task = (1.5 / 12)
-print('Курс:',name,',''всего задач:',homework,',''затрачено часов:',hours_for_homework,',''среднее время выполнения',time_for_task)
