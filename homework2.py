6f56fb4b2cbeee73da6b28fc509791181d8d82e2
Aleksey53 <vmax7777@yandex.ru>
2024-07-22 23:18:21 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+homework = 12
+hours_for_homework = 1.5
+name = 'Paython'
+time_for_task = hours_for_homework / homework
+print('Курс:',name,',''всего задач:',homework,',''затрачено часов:',hours_for_homework,',''среднее время выполнения',time_for_task)
