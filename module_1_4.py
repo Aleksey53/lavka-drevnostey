75bf7e4e7e02952a5ea1802c03055f0e982527fe
Aleksey53 <vmax7777@yandex.ru>
2024-07-31 19:40:39 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,11 @@
+my_string = input("Произвольный текст: ")
+print(my_string)
+number_of_letter = input("Количество символов в тексте: ")
+print(number_of_letter)
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ', ''))
+print(my_string[0])
+print(my_string[-1])
+
+
