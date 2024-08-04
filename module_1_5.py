f6df6a393870127b4dccba9cc5ed52420cd7c5a5
Aleksey53 <vmax7777@yandex.ru>
2024-08-04 12:55:53 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,7 @@
+tuple_immutable_var = (1, 3, 9, 'ветер', False, 23.7)
+print(tuple_immutable_var)
+# tuple_immutable_var[4] = True # Кортеж является неизменяемым объектoм
+mutable_list = [31, 'свет', True, 51, 13.5, 'nigth']
+mutable_list[2] = ('string')
+mutable_list[5] = 100
+print(mutable_list)
